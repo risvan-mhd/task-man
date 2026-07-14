accceaf1d65089db06a2e2fff02f58b4178c540f
Risvan Muhammad <risvan145k@gmail.com>
2026-07-14 22:28:31 +0530

    Implement create task

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("tasks/", views.task_create, name="task-create"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,22 +1,33 @@
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.http import require_POST
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from django_htmx.http import trigger_client_event
 
 from .models import Task
 
 
 def index(request: WSGIRequest) -> HttpResponse:
     tasks = Task.objects.all()
-    _ = tasks
-
-    # Dummy data
     context = {
-        "tasks": [
-            {"name": "Task 1", "completed": True},
-            {"name": "Task 2", "completed": False},
-            {"name": "Task 3", "completed": False},
-            {"name": "Task 4", "completed": False},
-            {"name": "Task 5", "completed": False},
-        ],
+        "tasks": tasks,
     }
+
     return render(request, "index.html", context)
+
+
+@require_POST
+def task_create(request: WSGIRequest) -> HttpResponse:
+    name = request.POST.get("task_name", "")
+    if name.strip() == "":
+        return HttpResponseBadRequest("Task name is required.")
+
+    Task.objects.create(name=name)
+    tasks = Task.objects.all()
+    context = {
+        "tasks": tasks,
+    }
+
+    respose = render(request, "index.html#task-list", context)
+    return trigger_client_event(respose, "clearTaskForm")
