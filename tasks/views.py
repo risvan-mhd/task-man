a876876a72be348064eb551a72d3e153e855edd6
Risvan Muhammad <risvan145k@gmail.com>
2026-07-14 21:10:02 +0530

    Render Tasks

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,21 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Task
+
 
 def index(request: WSGIRequest) -> HttpResponse:
-    return render(request, "index.html")
+    tasks = Task.objects.all()
+    _ = tasks
+
+    # Dummy data
+    context = {
+        "tasks": [
+            {"name": "Task 1", "completed": True},
+            {"name": "Task 2", "completed": False},
+            {"name": "Task 3", "completed": False},
+            {"name": "Task 4", "completed": False},
+            {"name": "Task 5", "completed": False},
+        ],
+    }
+    return render(request, "index.html", context)
