41d623d51d60e68ea38c779a2eb547ece4ffe211
Risvan Muhammad <risvan145k@gmail.com>
2026-07-14 20:03:25 +0530

    Add Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=256)
+    competed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
